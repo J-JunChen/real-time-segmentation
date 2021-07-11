df64077b7596597108c7a3ec0109f91a1efd9f6e
Jun-jieChen <cjj@hdu.edu.cn>
2021-07-11 14:59:38 +0800

    fork from open-mmlab

---
+++ configs/_base_/models/bisenetv1_r18.py
@@ -0,0 +1,58 @@
+# model settings
+# norm_cfg = dict(type='SyncBN', requires_grad=True)
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='open-mmlab://resnet18_v1c',
+    backbone=dict(
+        type='BiseNetV1',
+        depth=18,
+        out_indices=(0, 1, 2),
+        # dilations=(1, 1, 1, 1), # no dilations in BiseNet, so this line can be annotated
+        # strides=(1, 2, 1, 1), # need downsample for regular resnet, so this line can be annotated
+        norm_cfg=norm_cfg,
+        align_corners=False),
+    decode_head=dict(
+        type='FCNHead',
+        in_index=-1,  # Backbone stage index
+        in_channels=256,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=[
+        dict(
+            type='FCNHead',
+            in_index=-2,
+            in_channels=128,
+            channels=64,
+            num_convs=1,
+            concat_input=False,
+            dropout_ratio=0.1,
+            num_classes=19,
+            norm_cfg=norm_cfg,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+        dict(
+            type='FCNHead',
+            in_index=-3,
+            in_channels=128,
+            channels=64,
+            num_convs=1,
+            concat_input=False,
+            dropout_ratio=0.1,
+            num_classes=19,
+            norm_cfg=norm_cfg,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    ],
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
---
+++ mmseg/models/decode_heads/__init__.py
@@ -0,0 +1,14 @@
+from .ann_head import ANNHead
+from .aspp_head import ASPPHead
+from .cc_head import CCHead
+from .da_head import DAHead
+from .fcn_head import FCNHead
+from .gc_head import GCHead
+from .lraspp_head import LRASPPHead
+from .nl_head import NLHead
+from .psp_head import PSPHead
+
+__all__ = [
+    'FCNHead', 'PSPHead', 'ASPPHead', 'NLHead', 'GCHead', 'CCHead',
+    'ANNHead', 'DAHead', 'LRASPPHead'
+]
